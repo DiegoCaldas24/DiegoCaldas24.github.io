2e879c6dc68cfb325a2b6b1ca75f51278c530863
DiegoCaldas24 <140341633+DiegoCaldas24@users.noreply.github.com>
2024-07-11 23:25:01 -0500

    Update login.js

--- src/login.js
+++ src/login.js
@@ -1,21 +1,12 @@
-import { User } from './User';
-
 const button = document.getElementById('btn');
 const form = document.getElementById('loginForm');
 const usernameInput = document.getElementById('username');
 const passwordInput = document.getElementById('password');
 const errorMessage = document.getElementById('errorMessage');
 
-let us = new User()
 
 button.addEventListener('click', (event)=>{
     event.preventDefault();
-
-    us.getUsers().map(u => {
-        if(u.userName === 'Diego' && u.password === '123456'){
-            alert('Correcto')
-        }
-    })
 
     const username = usernameInput.value.trim();
     const password = passwordInput.value.trim();
