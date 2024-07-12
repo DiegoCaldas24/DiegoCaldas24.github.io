09d1c7303e6c6469d6bab0d71bd5ae821133cfa7
DiegoCaldas24 <140341633+DiegoCaldas24@users.noreply.github.com>
2024-07-11 23:31:02 -0500

    Update register.js

--- src/register.js
+++ src/register.js
@@ -1,18 +1,7 @@
-import {User} from './User.js';
-
-let user = new User();
-
 const createUser = async (evento)=>{
     evento.preventDefault();
     const userName = document.getElementById('userName').value.trim();
     const password = document.getElementById('password').value.trim();
-
-
-    if(user.addUser(userName,password)){
-        alert('usuario agregado con exito')
-    }else{
-        alert('ingrese otra vez los usuarios')
-    }
     
 }   
 
