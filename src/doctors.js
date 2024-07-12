9e47a7818164a7e4026e7bebc6d7fa33e8d5e090
DiegoCaldas24 <140341633+DiegoCaldas24@users.noreply.github.com>
2024-07-11 23:26:11 -0500

    Delete src/doctors.js

--- src/doctors.js
+++
@@ -1,7 +0,0 @@
-function getInfoDoctors() {
-    const infoDoctors= document.querySelector(".bunisessHours");
-    const doctorList = document.createElement("section");
-    doctorList.classList.add("hourDoctor");
-    
-    infoDoctors.append(doctorList);
-}
